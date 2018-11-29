d9131928f79cfd479d094fd39ee17563ae07a66a
Vladislav <miznikov.1995@mail.ru>
2018-11-29 16:12:14 +0500

    Начальный коммит

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'alc-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'homeAccounting';
-}
+export class AppComponent {}
---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { UsersService } from '../../shared/services/users.service';
+import { User } from '../../shared/models/user.model';
+
+@Component({
+  selector: 'alc-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  form: FormGroup;
+
+  constructor(private userService: UsersService) { }
+
+  ngOnInit() {
+    this.form = new FormGroup({
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'password': new FormControl(null, [Validators.required, Validators.minLength(6)])
+    });
+  }
+
+  onSubmit() {
+    const formData = this.form.value;
+    this.userService.getUserByEmail(formData.email).subscribe((user: User) => {
+        console.log(user);
+    });
+  }
+}
