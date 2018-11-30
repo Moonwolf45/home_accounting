cc5fc6af80a1dd3a08970c8b5ddbc487b11f8641
Vladislav <miznikov.1995@mail.ru>
2018-11-30 17:05:47 +0500

    Сделана верстка внутренних разделов системы

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AuthModule } from './auth/auth.module';
 import { AppRoutingModule } from './app-routing.module';
 import { UsersService } from './shared/services/users.service';
 import { AuthService } from './shared/services/auth.service';
+import { SystemModule } from './system/system.module';
 
 
 @NgModule({
@@ -18,6 +19,7 @@ import { AuthService } from './shared/services/auth.service';
     HttpClientModule,
     AuthModule,
     AppRoutingModule,
+    SystemModule
   ],
   providers: [
       UsersService,
---
+++ src/app/system/system.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+
+@Component({
+    selector: 'alc-system',
+    templateUrl: './system.component.html'
+})
+export class SystemComponent implements OnInit {
+
+    constructor (private router: Router) {}
+
+    ngOnInit() {
+        this.router.navigate(['/bill']);
+    }
+}
